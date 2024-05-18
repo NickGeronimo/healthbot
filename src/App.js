22ae83a1e39f5f541901514e29f30c237a4988d9
Nick <34172910+NickGeronimo@users.noreply.github.com>
2024-05-18 15:27:26 -0400

    deploy to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(process.env)
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [responding, setResponse] = useState("");
+    const [sentiment, setSentiment] = useState("");
+
+    async function callAwanAPI(){
+      await fetch("https://api.awanllm.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": API_KEY,
+        },
+        body: JSON.stringify({
+          "model": "Meta-Llama-3-8B-Instruct",
+          "messages": [
+            {"role": "user", "content": responding},
+    ],
+  })
+}).then((data) => {
+  return data.json();
+}).then((data) => {
+  setSentiment(data.choices[0].message.content)
+});
+    }
+
+    return (
+      <div className="App">
+        <div>
+          <textarea 
+            onChange={(e) => setResponse(e.target.value)}
+            placeholder='ask question'
+            cols={50}
+            rows={10}
+          />
+        </div>
+        <div>
+          <button onClick={callAwanAPI}> button</button>
+          {sentiment !== ""? 
+        <h3>This Is: {sentiment}</h3>
+        :
+        null  
+        }
+        </div>
+      </div>
+    );
+    }
 
 export default App;
